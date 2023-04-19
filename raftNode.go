a42d28f18db2d186e9c06b7a901a1c7a18ed41c5
Cheryl Wang <cherylwang@Cheryls-MacBook-Air.local>
2023-04-19 15:27:13 -0400

    client?

--- raftNode.go
+++ raftNode.go
@@ -73,6 +73,7 @@ var myPort string
 var mutex sync.Mutex // to lock global variables
 var electionTimeout *time.Timer
 var logs []LogEntry
+var lastAppliedIndex int
 
 // resetElectionTimeout resets the election timeout to a new random duration.
 // This function should be called whenever an event occurs that prevents the need for a new election,
@@ -268,6 +269,48 @@ func Heartbeat() {
 		}
 		heartbeatTimer.Reset(100 * time.Millisecond)
 	}
+}
+
+/*
+This function is designed to emulate a client reaching out to the
+server. Note that many of the realistic details are removed, for
+simplicity
+*/
+func ClientAddToLog() {
+	// In a realistic scenario, the client will find the leader node and communicate with it
+	// In this implementation, we are pretending that the client reached out to the server somehow
+	// But any new log entries will not be created unless the server/node is a leader
+	// isLeader here is a boolean to indicate whether the node is a leader or not
+	if isLeader {
+		// lastAppliedIndex here is an int variable that is needed by a node to store the value of the last index it used in the log
+		entry := LogEntry{lastAppliedIndex, currentTerm}
+		log.Println("Client communication created the new log entry at index " + strconv.Itoa(entry.Index))
+		lastAppliedIndex++
+		time.Sleep(5 * time.Second)
+
+		mutex.Lock()
+		arguments := AppendEntryArgument{
+			Term:     currentTerm,
+			LeaderID: selfID,
+			Address:  myPort,
+		}
+		mutex.Unlock()
+
+		for _, server := range serverNodes {
+			go func(server ServerConnection) {
+				reply := AppendEntryReply{}
+				server.rpcConnection.Call("RaftNode.AppendEntry", arguments, &reply)
+			}(server)
+		}
+		// Add rest of logic here
+		// HINT 1: using the AppendEntry RPC might happen here
+		//1) actual entry
+	}
+	/* HINT 2: force the thread to sleep for a good amount of time (less
+	   than that of the leader election timer) and then repeat the actions above.
+	   You may use an endless loop here or recursively call the function
+	*/
+	// HINT 3: you don’t need to add to the logic of creating new log entries, just handle the replication
 }
 
 func main() {
